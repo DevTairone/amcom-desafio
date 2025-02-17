3fa491a5130e633948014431f5e457ac035eb108
DevTairone <167230880+DevTairone@users.noreply.github.com>
2025-02-17 18:32:07 -0300

    create listener queue in RabbitMQ

--- src/main/java/amcom/desafio/orderms/listiner/OrderCreatedListner.java
+++ src/main/java/amcom/desafio/orderms/listiner/OrderCreatedListner.java
@@ -1,6 +1,9 @@
 package amcom.desafio.orderms.listiner;
 
+import amcom.desafio.orderms.config.RabbitMqConfig;
 import amcom.desafio.orderms.listiner.dto.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -10,8 +13,11 @@ import static amcom.desafio.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
 @Component
 public class OrderCreatedListner {
 
+    private final Logger logger = LoggerFactory.getLogger(RabbitMqConfig.class);
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message) {
+        logger.info("message consumed: {}", message);
 
     }
 }
